797bc7a4f87814669b55e9f220c1d9d8adcda617
rashmi11mokashi <rashmimokashi2099@gmail.com>
2024-03-16 13:56:15 +0900

    URL parsing error is there

--- crawler.test.js
+++ crawler.test.js
@@ -1,53 +1,62 @@
-const { normalizeURL, getURLsFromHtml } = require('./crawler.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
-
-
-test('normalizeURL strip protocol', () => {
-    const input = 'https://www.youtube.com/path/'
+test('normalizeURL protocol', () => {
+    const input = 'https://blog.boot.dev/path'
     const actual = normalizeURL(input)
-    const expected = 'www.youtube.com/path'
+    const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
-
 })
 
-test('normalizeURL strip slash', () => {
-    const input = 'https://www.youtube.com/path/'
+test('normalizeURL slash', () => {
+    const input = 'https://blog.boot.dev/path/'
     const actual = normalizeURL(input)
-    const expected = 'www.youtube.com/path'
+    const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
-
 })
 
-test('normalizeURL strip capitals', () => {
-    const input = 'https://www.YOUTube.com/path/'
+test('normalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path'
     const actual = normalizeURL(input)
-    const expected = 'www.youtube.com/path'
+    const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
-
 })
 
-test('normalizeURL strip http', () => {
-    const input = 'https://www.youtube.com/path/'
+test('normalizeURL http', () => {
+    const input = 'http://BLOG.boot.dev/path'
     const actual = normalizeURL(input)
-    const expected = 'www.youtube.com/path'
+    const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
-
 })
 
-test('getURLsFromHtml ', () => {
-    const inputHtmlBody = `
-    <html>
-        <body>
-            <a href="https://www.youtube.com/">
-                youtube homepage
-            </a>
-        </body>
-    </html>
-    `
-    const inputBaseURL = "https://www.youtube.com/path/"
-    const actual = getURLsFromHtml(inputHtmlBody, inputBaseURL)
-    const expected = ['https://www.youtube.com/']
+test('getURLsFromHTML absolute', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = ['https://blog.boot.dev/']
     expect(actual).toEqual(expected)
+})
 
+test('getURLsFromHTML relative', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = ['https://blog.boot.dev/path/one']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = ['https://blog.boot.dev/path/one', 'https://other.com/path/one']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML handle error', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = []
+    expect(actual).toEqual(expected)
 })
--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const { crawlPage } = require('./crawler.js')
+const { crawlPage } = require('./crawl.js')
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
-        console.log("No website provided")
-        process.exit(1)
+        console.log('no website provided')
     }
-    for (const arg of process.argv) {
-        console.log(arg)
+    if (process.argv.length > 3) {
+        console.log('too many arguments provided')
     }
-    const baseURL = process.argv[2]
-    console.log(`Starting crawling of ${baseURL}`)
-    crawlPage(baseURL)
 
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of: ${baseURL}...`)
+
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    console.log(pages)
 }
 
 main()
